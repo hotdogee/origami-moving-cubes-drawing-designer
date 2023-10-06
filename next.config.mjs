d1c028f5aa8f6fe3ec5b4980957d26b3b69fbc29
Han Lin <hotdogee@gmail.com>
2023-10-07 02:45:12 +0800

    Offline support

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+import withPWAInit from '@ducanh2912/next-pwa'
+
+const withPWA = withPWAInit({
+  dest: 'public',
+})
+
+export default withPWA(nextConfig)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,49 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const APP_NAME = 'Origami Cubes'
+const APP_DEFAULT_TITLE = 'Origami Moving Cubes Drawing Designer'
+const APP_TITLE_TEMPLATE = '%s - Origami Cubes'
+const APP_DESCRIPTION =
+  'Origami Moving Cubes Drawing Designer by Han Lin (hotdogee [at] gmail [dot] com)'
+
 export const metadata: Metadata = {
-  title: 'Origami Moving Cubes Drawing Designer',
-  description: 'Origami Moving Cubes Drawing Designer by Han Lin (hotdogee [at] gmail [dot] com)',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#f3f4f6' },
     { media: '(prefers-color-scheme: dark)', color: '#1f2937' },
   ],
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_NAME,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
