f92bc81959b2f919910e9db218664375f01a07ed
Han Lin <hotdogee@gmail.com>
2023-10-06 08:31:53 +0800

    Bug fix

--- src/app/grid/page.tsx
+++ src/app/grid/page.tsx
@@ -6,18 +6,18 @@ export default function TestPage() {
     var canvas = document.querySelectorAll('canvas')
     for (var i = 0; i < canvas.length; i++) {
       let ctx = canvas[i].getContext('2d')
-      ctx.fillStyle = '#999'
-      ctx.fillRect(0, 0, 900, 900)
+      ctx!.fillStyle = '#999'
+      ctx!.fillRect(0, 0, 900, 900)
     }
   }, [])
   return (
-    <div className="bg-red-200 h-screen grid grid-rows-[40px_1fr_1fr_40px]">
+    <div className="grid h-screen grid-rows-[40px_1fr_1fr_40px] bg-red-200">
       <div className="bg-orange-200">header</div>
-      <div className="bg-blue-200 min-h-0">
-        <canvas className="object-contain h-full w-full" width={`900px`} height={`900px`}></canvas>
+      <div className="min-h-0 bg-blue-200">
+        <canvas className="h-full w-full object-contain" width={`900px`} height={`900px`}></canvas>
       </div>
-      <div className="bg-green-200 min-h-0">
-        <canvas className="object-contain h-full w-full" width={`900px`} height={`900px`}></canvas>
+      <div className="min-h-0 bg-green-200">
+        <canvas className="h-full w-full object-contain" width={`900px`} height={`900px`}></canvas>
       </div>
       <div className="bg-purple-200">toolbar</div>
     </div>
