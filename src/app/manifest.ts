2e7a31246a4558207a56e318e0f126ab6b90cf1a
Han Lin <hotdogee@gmail.com>
2025-06-02 18:52:49 +0800

    feat: add new icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,8 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Origami Moving Cubes Drawing Designer',
     short_name: 'Origami Cubes',
-    description: 'Origami Moving Cubes Drawing Designer by Han Lin (hotdogee [at] gmail [dot] com)',
+    description:
+      'Origami Moving Cubes Drawing Designer by Han Lin (hotdogee [at] gmail [dot] com)',
     start_url: '/',
     scope: '/',
     display: 'fullscreen',
@@ -12,16 +13,14 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#1f2937',
     theme_color: '#1f2937',
     icons: [
-      { src: '/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
-      { src: '/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
       {
-        src: '/icons/maskable_icon_x192.png',
+        src: '/icons/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icons/maskable_icon_x512.png',
+        src: '/icons/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
